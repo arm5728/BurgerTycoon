7982c8807f0543291e159c49b74bdf9df428589a
Adrian Melendez Relli <42785149+arm5728@users.noreply.github.com>
2019-01-11 16:24:59 -0600

    Update Report.java

--- Report.java
+++ Report.java
@@ -1,4 +1,25 @@
+import java.util.TreeMap;
 
-public class Report {
+public class Report {	
+	public int revenue;
+	public int expenses;
+	public TreeMap<String, String> restaurantPerformance;
+	
+	public Report() {
+		revenue = 0;
+		expenses = 0;
+		restaurantPerformance = new TreeMap<String, String>();
+	}
+	
+	public void printReport() {
+		//Main
+		System.out.println("Yesterday's Profit/Loss: " + (revenue - expenses));
+		System.out.println("Revenue: " + revenue);
+		System.out.println("Expenses: " + expenses);
 
+		//Reports
+		for (String location : restaurantPerformance.keySet()) {
+			System.out.println(location + ": " + restaurantPerformance.get(location));
+		}
+	}	
 }
