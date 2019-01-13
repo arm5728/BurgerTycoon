99a69bee4a1093de372a6877945915acddcf40d0
Adrian Melendez Relli <42785149+arm5728@users.noreply.github.com>
2019-01-12 20:25:58 -0600

    Update Report.java

--- Report.java
+++ Report.java
@@ -13,13 +13,15 @@ public class Report {
 	
 	public void printReport() {
 		//Main
-		System.out.println("Yesterday's Profit/Loss: " + (revenue - expenses));
+		System.out.println("~~ YESTERDAY'S REPORT ~~");
+		System.out.println("Profit/Loss: " + (revenue - expenses));
 		System.out.println("Revenue: " + revenue);
 		System.out.println("Expenses: " + expenses);
+		System.out.println("Restaurant Performance: ");
 
 		//Reports
 		for (String location : restaurantPerformance.keySet()) {
-			System.out.println(location + ": " + restaurantPerformance.get(location));
+			System.out.println("\t" + location + ": " + restaurantPerformance.get(location));
 		}
 	}	
 }
