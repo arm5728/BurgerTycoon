2eea13077654f4689581987ae7cec10cd70e1b5f
Adrian Melendez Relli <42785149+arm5728@users.noreply.github.com>
2019-01-27 20:41:45 -0600

    Update Report.java

--- Report.java
+++ Report.java
@@ -1,14 +1,17 @@
+import java.util.ArrayList;
 import java.util.TreeMap;
 
 public class Report {	
 	public int revenue;
 	public int expenses;
 	public TreeMap<String, String> restaurantPerformance;
+	public ArrayList<String> news;
 	
 	public Report() {
 		revenue = 0;
 		expenses = 0;
 		restaurantPerformance = new TreeMap<String, String>();
+		news = new ArrayList<String>();
 	}
 	
 	public void printReport() {
@@ -17,11 +20,19 @@ public class Report {
 		System.out.println("Profit/Loss: " + (revenue - expenses));
 		System.out.println("Revenue: " + revenue);
 		System.out.println("Expenses: " + expenses);
+		
+		//Restaurant Reports
 		System.out.println("Restaurant Performance: ");
-
-		//Reports
 		for (String location : restaurantPerformance.keySet()) {
 			System.out.println("\t" + location + ": " + restaurantPerformance.get(location));
+		}
+		
+		//News
+		if (!news.isEmpty()) {
+			System.out.println("News:");
+			for (String newsItem: news) {
+				System.out.println("\t" + newsItem);			
+			}
 		}
 	}	
 }
